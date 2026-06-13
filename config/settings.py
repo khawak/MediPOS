27bcf52805b955e25e2233efea7b1192b7bc4e7b
Khawaja Ashraful Hawak <44132573+khawak@users.noreply.github.com>
2026-06-13 22:58:35 +0600

    postgres

--- config/settings.py
+++ config/settings.py
@@ -4,9 +4,12 @@ MediPOS Settings.
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+import dj_database_url
 from pathlib import Path
 
 from decouple import Csv, config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,10 +83,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # ── Database ──────────────────────────────────────────────────────────────
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': config('DB_ENGINE', default='django.db.backends.sqlite3'),
-        'NAME': BASE_DIR / config('DB_NAME', default='db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': config('DB_ENGINE', default='django.db.backends.sqlite3'),
+    #     'NAME': BASE_DIR / config('DB_NAME', default='db.sqlite3'),
+    # }
+    "default": dj_database_url.parse(
+        os.environ.get("POSTGRES_URL_NON_POOLING")
+    )
 }
 
 # ── Custom User Model ─────────────────────────────────────────────────────
